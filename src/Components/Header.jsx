65122caa203c1e55c778d142e9325083aeaae885
Shivangi020 <me.shivasingh.0987@gmail.com>
2023-03-24 13:09:19 +0530

    Adding logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { GiTakeMyMoney } from 'react-icons/gi';
 
 function Header() {
   return (
-    <h2>
-        Money Miser
+    <div className='header'>
+      <GiTakeMyMoney className='money-icon'/>
+      <h2>
+     Money Miser
     </h2>
+    </div>
+    
   )
 }
 
